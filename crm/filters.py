3dd0f1ed252549fe7fe7c5852514f522eb52c40a
Louack <loic.briset@hotmail.fr>
2021-09-27 09:22:22 +0200

    updated crm api filters

--- crm/filters.py
+++ crm/filters.py
@@ -4,6 +4,15 @@ from .models import Client, Contract, Event
 
 
 class ClientFilter(filters.FilterSet):
+
+    min_date_created = filters.DateFilter(
+        field_name="date_created",
+        lookup_expr='gte')
+
+    max_date_created = filters.DateFilter(
+        field_name="date_created",
+        lookup_expr='lte')
+
     last_name_contains = filters.CharFilter(
         field_name="last_name",
         lookup_expr='icontains'
@@ -19,10 +28,18 @@ class ClientFilter(filters.FilterSet):
         lookup_expr='icontains'
     )
 
-    company_contains = filters.DateFilter(
+    company_contains = filters.CharFilter(
         field_name="company",
         lookup_expr='icontains'
     )
+
+    sort_by = filters.CharFilter(
+        method='filter_sort_by',
+    )
+
+    def filter_sort_by(self, queryset, name, value):
+        values = value.lower().split(',')
+        return queryset.order_by(*values)
 
     class Meta:
         model = Client
@@ -35,11 +52,30 @@ class ClientFilter(filters.FilterSet):
             'mobile',
             'company',
             'date_created',
-            'date_updated'
+            'date_updated',
+            'min_date_created',
+            'max_date_created',
+            'sort_by'
         ]
 
 
 class ContractFilter(filters.FilterSet):
+    min_date_created = filters.DateFilter(
+        field_name="date_created",
+        lookup_expr='gte')
+
+    max_date_created = filters.DateFilter(
+        field_name="date_created",
+        lookup_expr='lte')
+
+    sort_by = filters.CharFilter(
+        method='filter_sort_by',
+    )
+
+    def filter_sort_by(self, queryset, name, value):
+        values = value.lower().split(',')
+        return queryset.order_by(*values)
+
     class Meta:
         model = Contract
         fields = [
@@ -48,15 +84,34 @@ class ContractFilter(filters.FilterSet):
             'amount',
             'payment_due',
             'date_created',
-            'date_updated'
+            'date_updated',
+            'min_date_created',
+            'max_date_created',
+            'sort_by'
         ]
 
 
 class EventFilter(filters.FilterSet):
+    min_date_created = filters.DateFilter(
+        field_name="date_created",
+        lookup_expr='gte')
+
+    max_date_created = filters.DateFilter(
+        field_name="date_created",
+        lookup_expr='lte')
+
     notes_contains = filters.CharFilter(
         field_name="notes",
         lookup_expr='icontains'
     )
+
+    sort_by = filters.CharFilter(
+        method='filter_sort_by',
+    )
+
+    def filter_sort_by(self, queryset, name, value):
+        values = value.lower().split(',')
+        return queryset.order_by(*values)
 
     class Meta:
         model = Event
@@ -66,5 +121,8 @@ class EventFilter(filters.FilterSet):
             'notes',
             'event_date',
             'date_created',
-            'date_updated'
+            'date_updated',
+            'min_date_created',
+            'max_date_created',
+            'sort_by'
         ]
